cbab11866e4e2bc5429e207ffd685d51d10d4f6b
jared1158 <jaredlns@gmail.com>
2018-07-03 13:41:11 -0700

    update

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetAdapter.java
@@ -1,21 +1,57 @@
 package com.codepath.apps.restclienttemplate;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import java.util.List;
 
 /**
  * Created by jared1158 on 7/2/18.
  */
 
-public class TweetAdapter {
+public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder> {
+    private List<Tweet> mTweets;
 
+    Context context;
     //pass in the tweets array in the constructor
-
+    public TweetAdapter(List<Tweet> tweets) {
+        mTweets = tweets;
+    }
     //for each row, inflate the layout and cache references into viewholder
 
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View tweetView = inflater.inflate(R.layout.item_tweet, parent, false);
+        ViewHolder viewHolder = new ViewHolder(tweetView);
+        return viewHolder;
+    }
+
     //bind the values based on the position of the element
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        //get the data according to the position
+        Tweet tweet = mTweets.get(position);
+
+        //populate the views according to this data
+        holder.tvUsername.setText(tweet.user.name);
+        holder.tvBody.setText(tweet.body);
+
+        Glide.with(context).load(tweet.user.profileImageUrl).into(holder.ivProfileImage);
+    }
 
     //create viewholder class
 
@@ -32,6 +68,12 @@ public class TweetAdapter {
 
             ivProfileImage = (ImageView) itemView.findViewById(R.id.ivProfileImage);
             tvUsername = (TextView) itemView.findViewById(R.id.tvUserName);
+            tvBody = (TextView) itemView.findViewById(R.id.tvBody);
         }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTweets.size();
     }
 }
